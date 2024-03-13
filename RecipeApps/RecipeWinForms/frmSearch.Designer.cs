4552f764654acdb9f8ba786cb492c7a92880944f
Chanybeck <chanybeck01@gmail.com>
2024-03-13 06:20:43 -0400

    Recipe setup

---
+++ RecipeApps/RecipeWinForms/frmSearch.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace RecipeWinForms
+{
+    partial class frmSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tblMain = new TableLayoutPanel();
+            tblSearch = new TableLayoutPanel();
+            lblRecipeName = new Label();
+            txtRecipeName = new TextBox();
+            btnSearch = new Button();
+            gRecipes = new DataGridView();
+            tblMain.SuspendLayout();
+            tblSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)gRecipes).BeginInit();
+            SuspendLayout();
+            // 
+            // tblMain
+            // 
+            tblMain.ColumnCount = 1;
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tblMain.Controls.Add(tblSearch, 0, 0);
+            tblMain.Controls.Add(gRecipes, 0, 1);
+            tblMain.Dock = DockStyle.Fill;
+            tblMain.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tblMain.Location = new Point(0, 0);
+            tblMain.Name = "tblMain";
+            tblMain.RowCount = 2;
+            tblMain.RowStyles.Add(new RowStyle());
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tblMain.Size = new Size(768, 558);
+            tblMain.TabIndex = 0;
+            // 
+            // tblSearch
+            // 
+            tblSearch.AutoSize = true;
+            tblSearch.ColumnCount = 3;
+            tblSearch.ColumnStyles.Add(new ColumnStyle());
+            tblSearch.ColumnStyles.Add(new ColumnStyle());
+            tblSearch.ColumnStyles.Add(new ColumnStyle());
+            tblSearch.Controls.Add(lblRecipeName, 0, 0);
+            tblSearch.Controls.Add(txtRecipeName, 1, 0);
+            tblSearch.Controls.Add(btnSearch, 2, 0);
+            tblSearch.Location = new Point(3, 3);
+            tblSearch.Name = "tblSearch";
+            tblSearch.RowCount = 1;
+            tblSearch.RowStyles.Add(new RowStyle());
+            tblSearch.Size = new Size(295, 37);
+            tblSearch.TabIndex = 0;
+            // 
+            // lblRecipeName
+            // 
+            lblRecipeName.Anchor = AnchorStyles.Left;
+            lblRecipeName.AutoSize = true;
+            lblRecipeName.Location = new Point(3, 8);
+            lblRecipeName.Name = "lblRecipeName";
+            lblRecipeName.Size = new Size(102, 21);
+            lblRecipeName.TabIndex = 0;
+            lblRecipeName.Text = "Recipe Name";
+            // 
+            // txtRecipeName
+            // 
+            txtRecipeName.Location = new Point(111, 3);
+            txtRecipeName.Name = "txtRecipeName";
+            txtRecipeName.Size = new Size(100, 29);
+            txtRecipeName.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Anchor = AnchorStyles.Left;
+            btnSearch.AutoSize = true;
+            btnSearch.Location = new Point(217, 3);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(75, 31);
+            btnSearch.TabIndex = 2;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // gRecipes
+            // 
+            gRecipes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gRecipes.Dock = DockStyle.Fill;
+            gRecipes.Location = new Point(3, 46);
+            gRecipes.Name = "gRecipes";
+            gRecipes.RowTemplate.Height = 25;
+            gRecipes.Size = new Size(762, 509);
+            gRecipes.TabIndex = 1;
+            // 
+            // frmSearch
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(768, 558);
+            Controls.Add(tblMain);
+            Name = "frmSearch";
+            Text = "frmSearch";
+            tblMain.ResumeLayout(false);
+            tblMain.PerformLayout();
+            tblSearch.ResumeLayout(false);
+            tblSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)gRecipes).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tblMain;
+        private TableLayoutPanel tblSearch;
+        private Label lblRecipeName;
+        private TextBox txtRecipeName;
+        private Button btnSearch;
+        private DataGridView gRecipes;
+    }
+}
---
+++ RecipeApps/RecipeWinForms/frmSearch.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecipeWinForms
+{
+    public partial class frmSearch : Form
+    {
+        public frmSearch()
+        {
+            InitializeComponent();
+            btnSearch.Click += BtnSearch_Click;
+        }
+
+        private void SearchForRecipes(string recipename)
+        {
+            string sql = "select * from Recipe r where r.recipename is like '%" + recipename + "%'";
+            Debug.Print(sql);
+            DataTable dt = GetDataTable(sql);
+            gRecipes.DataSource = dt;
+           
+        }
+        private string GetConnectionString()
+        {
+            var s = "Server=.\\SQLExpress;Database=HeartyHearthDB;Trusted_Connection=True";
+            return s;
+        }
+
+        private DataTable GetDataTable(string sqlstatement)
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = GetConnectionString();
+            conn.Open();
+            SqlCommand cmd = new();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            SqlDataReader dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+        private void BtnSearch_Click(object? sender, EventArgs e)
+        {
+            SearchForRecipes(txtRecipeName.Text);
+        }
+        
+    }
+}
