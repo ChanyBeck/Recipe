a224715b7ccfebb7fb3137f0ae9f0d8444f7f8b6
Chanybeck <chanybeck01@gmail.com>
2024-03-18 16:28:16 -0400

    Data test

--- Database Files/4 Recipe Data without related Records.sql
+++ Database Files/4 Recipe Data without related Records.sql
@@ -1,3 +1,13 @@
-insert Recipe(RecipeName, Calories)
-select 'Toast', 100
-union select 'Cherry Pie', 150
+;
+with x as(
+    select UserName = 'GRamsay', CuisineType = 'English', RecipeName = 'Green Salad', Calories = 100, DatePublished = getdate(), DateArchived = getdate()
+    union select 'GFieri', 'French', 'French Toast', 80, getdate(), null
+    union select 'MCaines', 'English', 'Cherry Pie', 200, null, null
+)
+insert Recipe(UsersId, CuisineId, RecipeName, Calories, DatePublished, DateArchived)
+select u.UsersId, c.CuisineId, x.RecipeName, x.Calories, x.DatePublished, x.DateArchived
+from x
+join Users u
+on x.UserName = u.UserName
+join Cuisine c 
+on x.CuisineType = c.CuisineType
