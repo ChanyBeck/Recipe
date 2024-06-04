032d7bdd8c7cdc4161faad51657587f4eaea1432
Chanybeck <chanybeck01@gmail.com>
2024-06-04 06:43:45 -0400

    RecipeSprocs

--- 3 Sprocs/UsersGet.sql
+++ 3 Sprocs/UsersGet.sql
@@ -1,11 +1,18 @@
-create or alter procedure dbo.UsersGet(@UsersId int = 0, @All bit = 0, @UserName varchar(50) = '')
+create or alter procedure dbo.UsersGet(
+    @UsersId int = 0, 
+    @All bit = 0,     
+    @Message varchar(500) = ''  output
+)  
 as 
 begin 
-    select @UserName = nullif(@UserName, '')
-    select * 
-    from Users u 
-    where u.UsersId = @UsersId 
-    or @All = 1
-    or u.UserName like '%' + @UserName + '%'
-end
+    declare @return int = 0
 
+	select @All = isnull(@All,0), @UsersId = isnull(@UsersId,0)
+
+	select u.UsersId, u.UserName
+	from Users u
+	where u.UsersId = @UsersId
+	or @All = 1
+	order by u.UserName
+end
+go
